4639d070f95b069f18e11e707cde192eb017c9e6
vincentrann <vincentrann@github.com>
2021-06-02 19:58:02 -0400

    Tweaked web scraping functions, texts are now added to AllTexts.txt

--- WebScapeTest.py
+++ WebScapeTest.py
@@ -3,17 +3,24 @@ from bs4 import BeautifulSoup
 
 main_url = 'https://data.typeracer.com/pit/texts'
 page = requests.get(main_url)
-all_texts = []
+all_quotes = []
 
 def getdata(url):
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
 
 def gettext(url):
-    soup = BeautifulSoup(page.content, 'html.parser')
-    return soup
+    text_url = url
+    text_page = requests.get(text_url)
+    soup = BeautifulSoup(text_page.content, 'html.parser')
+    words = soup.find('div', {'class':'fullTextStr'})
+    if text_page.text.count('English') == 1 and words != None:
+        return words.text
+    else:
+        return None
 
-def textpage(soup):
+
+def database(soup):
     containers = soup.findAll('td', {"style":"text-align: left;"})
     urls = []
 
@@ -24,14 +31,33 @@ def textpage(soup):
 
     return urls
 
+def check_string(file_name, string):
+    with open(file_name, 'r') as read_obj:
+        for line in read_obj:
+            if string in line:
+                return True
+    return False
+
 
 data = getdata(main_url)
-urls = textpage(data)
+urls = database(data)
 
-'''for url in urls:
-    all_texts.append(gettext(url))
+for i in range(0,10):
+    all_quotes.append(gettext(urls[i]))
 
-print(all_texts)'''
+with open('AllTexts.txt', 'a') as f:
+    for line in all_quotes:
+        if line != None and not check_string('AllTexts.txt', line):
+            f.write(line)
+            f.write('\n')
+
+
+
+
+
+
+
+
 
 
 
